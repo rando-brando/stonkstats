68ad4bf6ca2a8c58978c184a496d6f9b1af09eed
rando-brando <branrich.services+github@icloud.com>
2023-06-11 19:42:46 +0000

    add dashboard layouts

---
+++ stonkstats/dashboard/layouts.py
@@ -0,0 +1,31 @@
+stock_chart_layout = {
+    'template': 'cyborg',
+    'font_color': 'gray',
+    'paper_bgcolor': 'rgba(0,0,0,0)',
+    'plot_bgcolor': 'rgba(0,0,0,0)',
+    'dragmode': 'pan',
+    'margin': {
+        'b': 20,
+        't': 20,
+        'l': 20,
+        'r': 20
+    },
+    'hovermode': 'x unified',
+    'hoverdistance': 20,
+    'xaxis': {
+        'showspikes': True, 
+        'spikemode': 'across', 
+        'spikesnap': 'cursor', 
+        'spikecolor': 'gray', 
+        'spikedash': 'dash', 
+        'spikethickness': -2
+    },
+    'yaxis': {
+        'showspikes': True, 
+        'spikemode': 'across', 
+        'spikesnap': 'cursor', 
+        'spikecolor': 'gray', 
+        'spikedash': 'dash', 
+        'spikethickness': -2
+    }
+}
