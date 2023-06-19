4d6c02fa9e54724b3c86a121608ae0884555baac
rando-brando <branrich.services+github@icloud.com>
2023-06-19 17:24:47 +0000

    add dynamodb

---
+++ stonkly/data/dynamodb.py
@@ -0,0 +1,10 @@
+import boto3
+import os
+
+dynamodb = boto3.client(
+    'dynamodb',
+    aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+    aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
+    region_name=os.getenv('AWS_REGION')
+
+)
