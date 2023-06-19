6ba3d27ae538bad3e2612590ad0b962bbd299856
rando-brando <branrich.services+github@icloud.com>
2023-06-19 22:42:55 +0000

    fix volume paths

--- stonkly/data/dynamodb.py
+++ stonkly/data/dynamodb.py
@@ -4,15 +4,10 @@ import os
 
 class DynamoDB:
     def __init__(self):
-        self.resource = boto3.resource(
-            'dynamodb',
-            aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
-            aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
-            region_name=os.getenv('AWS_REGION')
-        )
+        self.resource = boto3.resource('dynamodb')
         self.available_symbols = self.resource.Table('available_symbols')
 
-    def put_items(table, )
-    with table.batch_writer() as batch:
-        for _ in range(1000000):
-            batch.put_item(Item={'HashKey': '...', 'Otherstuff': '...'})
+    #def put_items(table, )
+    #with table.batch_writer() as batch:
+    #    for _ in range(1000000):
+    #        batch.put_item(Item={'HashKey': '...', 'Otherstuff': '...'})
